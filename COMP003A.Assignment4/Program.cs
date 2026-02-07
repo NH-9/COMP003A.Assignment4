baceaa5c0711b1076a1535f858871641bbabc9c0
NH <noahhulett@my.whccd.edu>
2026-02-06 18:56:08 -0800

    display menu options

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -6,9 +6,18 @@ namespace COMP003A.Assignment4
     {
         static void Main(string[] args)
         {
+            // this is used to contextualize the program and to seperate the the loop condition from the saftey limit condition
+            // if the initial number of steps is static then only one of those 2 conditions would ever stop the program making it redundant
             Console.WriteLine("---Stair Climbing Simulator---");
             Console.Write("How many steps would you like to climb?: ");
             int goal = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < goal; i++)
+            {
+                Console.WriteLine("\n1 - Take A Step\n2 - Count Progress\n3 - Stop Climbing");
+                Console.Write("Enter choice: ");
+                int choice = int.Parse(Console.ReadLine());
+            }
         }
     }
 }
