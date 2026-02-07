355117d22c91cd0dbff60b648d656161302a3c9f
NH <noahhulett@my.whccd.edu>
2026-02-06 20:08:14 -0800

    saftey limit enforced; menu functionality extended

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -12,18 +12,26 @@ namespace COMP003A.Assignment4
             Console.Write("How many steps would you like to climb?: ");
             int goal = int.Parse(Console.ReadLine());
             int progress = 0;
+            string menu = "1 - Take A Step\n2 - Count Progress\n3 - Stop Climbing\n4 - Display Menu";
 
+            Console.WriteLine($"\n{menu}");
             do
             {
-                Console.WriteLine("\n1 - Take A Step\n2 - Count Progress\n3 - Stop Climbing");
-                Console.Write("Enter choice: ");
+                Console.Write("\nEnter choice: ");
                 int choice = int.Parse(Console.ReadLine());
 
                 switch (choice)
                 {
                     case 1:
                         {
+                            if (progress >= 17) // using the standard step height of 7 inches this would be about 10 feet high
+                            {
+                                Console.WriteLine("It is too dangerous to go any higher.");
+                                continue;
+                            }
+                            
                             progress++;
+                            
                             if (progress == 4 || progress == 13) // 4 and 13 are considered unlucky numbers in certain cultures
                             {
                                 Console.WriteLine("This step is unlucky and has been skipped over.");
@@ -46,10 +54,20 @@ namespace COMP003A.Assignment4
                         {
                             Console.WriteLine("You have stoped climbing the staircase.");
                             progress = goal;
+                            continue;
+                        }
+
+                    case 4:
+                        {
+                            Console.WriteLine(menu);
                             break;
                         }
                 }
                 
+                if (progress >= goal)
+                {
+                    Console.WriteLine("You have completed your climb!");
+                }
             }
             while (progress < goal);
         }
